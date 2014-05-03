78c9835d1f6ee94093eaa4c27fd0845fd85c6bb1
Jerad Phelps <jerad.phelps@gmail.com>
2014-05-02 21:01:21 -0500

    Don't require an access code unless we're in prod

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,7 +3,9 @@ module API
 
     helpers do
       def authenticate! access_token
-        error!('401 Unauthorized', 401) unless AccessToken.find_by(:token => access_token)
+        if Rails.env.production?
+          error!('401 Unauthorized', 401) unless AccessToken.find_by(:token => access_token)
+        end
       end
     end
   
